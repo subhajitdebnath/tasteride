38de5703e9454b89450e9719dd2cdebc03497d58
SubhajitD <subhajitdebnath.999@gmail.com>
2023-06-20 18:46:05 +0530

    initial changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tasteride';
+
+
+  foods: any[] = [
+    {
+      id: 1,
+      name: 'Pizza'
+    },
+    {
+      id: 2,
+      name: 'Biriyani'
+    }
+  ]
 }
